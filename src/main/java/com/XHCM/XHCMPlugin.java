4f7edd15e30abf0cde9e93c0c2134f304dafe8c0
Yookiop <yookiop@hotmail.com>
2025-04-26 15:40:59 +0200

    test

--- src/main/java/com/XHCM/XHCMPlugin.java
+++ src/main/java/com/XHCM/XHCMPlugin.java
@@ -1,11 +1,18 @@
 package com.XHCM;
 
 import com.google.inject.Provides;
+
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
+//import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.Skill;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -25,6 +32,9 @@ public class XHCMPlugin extends Plugin
 
 	@Inject
 	private XHCMConfig config;
+	private Boolean firstRun = true;
+	private Boolean playerIsDead = false;
+	private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
 	@Override
 	protected void startUp() throws Exception
@@ -43,7 +53,30 @@ public class XHCMPlugin extends Plugin
 	{
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			if(firstRun) {
+				executorService.scheduleAtFixedRate(this::checkForDeath, 10, 1, TimeUnit.SECONDS);
+				firstRun = false;
+			}
+		}
+
+		if (gameStateChanged.getGameState() == GameState.LOGIN_SCREEN) {
+			firstRun = true;
+		}
+	}
+
+
+	private void checkForDeath() {
+		if (client.getGameState() == GameState.LOGGED_IN) {
+			int currentHP = client.getBoostedSkillLevel(Skill.HITPOINTS);
+
+			if (currentHP <= 0) {
+				if(!playerIsDead) {
+					playerIsDead = true;
+					//Text command replacement you died
+				}
+			} else {
+				playerIsDead = false;
+			}
 		}
 	}
 
