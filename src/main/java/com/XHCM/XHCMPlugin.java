07db20b30707948e4f4901e31513fb66a4e8e1ef
Yookiop <yookiop@hotmail.com>
2025-04-27 18:22:46 +0200

    removed logging for working version

--- src/main/java/com/XHCM/XHCMPlugin.java
+++ src/main/java/com/XHCM/XHCMPlugin.java
@@ -329,9 +329,6 @@ public class XHCMPlugin extends Plugin
             int currentHP = client.getBoostedSkillLevel(Skill.HITPOINTS);
             boolean currentlyDead = currentHP <= 0;
 
-            log.info("currentHP: {}", client.getBoostedSkillLevel(Skill.HITPOINTS));
-            log.info("PermanentDeath: {}", config.permanentDeath());
-
             // If player is dead and this hasn't been saved as permanent yet
             if (currentlyDead && !config.permanentDeath())
             {
@@ -356,8 +353,6 @@ public class XHCMPlugin extends Plugin
             }
             else if (!currentlyDead && !config.permanentDeath())
             {
-                log.info("Player is alive.");
-                log.info("currentHP: {}", currentHP);
             }
         }
     }
